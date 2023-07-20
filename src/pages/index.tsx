1995b640f75b13b0efe95d980e72a829f5f70904
filipegbessa <filipegbessa@gmail.com>
2023-07-20 19:21:47 -0300

    fix(): fix pages and move page folder to src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,14 @@ import Box from "@/components/layout/Box";
 export default function Home() {
   return (
     <Box>
-      <Typography>
-        Testando
+      <Typography
+        sx={{
+          fontSize: '20px',
+          fontWeight: 'bold',
+          color: 'primary.main',
+        }}
+      >
+        Testando  
       </Typography>
     </Box>    
   );
